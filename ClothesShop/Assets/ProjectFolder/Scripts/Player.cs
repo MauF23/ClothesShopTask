0319ada1b46f39bf0d1797e78def0da60663d3a5
MauF23 <mauriciofuentesw23e@gmail.com>
2024-05-07 17:19:52 -0600

    Project Player Controller, Animator

---
+++ ClothesShop/Assets/ProjectFolder/Scripts/Player.cs
@@ -0,0 +1,88 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Sirenix.OdinInspector;
+using Unity.Burst;
+
+public class Player : MonoBehaviour
+{
+    public float speed;
+    public Rigidbody2D rigidbody2D;
+    public Animator animator;
+    private float movementX, movementY;
+    private Vector2 movement;
+    [ReadOnly]
+    public bool canMove;
+    private Vector3 originalScale;
+    private InputManager inputManager;
+    private const string animatorSpeed = "speed";
+
+    public static Player instance;
+
+    void Awake()
+    {
+        if(instance == null)
+        {
+            instance = this;
+        }
+    }
+    void Start()
+    {
+        ToggleMovement(true);
+        originalScale = transform.localScale;
+        inputManager = InputManager.Instance;
+    }
+
+    private void Update()
+    {
+        MovementInput();
+    }
+    void FixedUpdate()
+    {
+        if (canMove)
+        {
+            Movement();
+        }
+    }
+
+    private void MovementInput()
+    {
+        movementX = inputManager.MovementVector().x;
+        movementY = inputManager.MovementVector().y;
+    }
+
+    private void Movement()
+    {
+        movement = new Vector2(movementX, movementY);
+        movement.Normalize();
+        rigidbody2D.velocity = movement * speed;
+
+        animator.SetFloat(animatorSpeed, rigidbody2D.velocity.sqrMagnitude);
+        if (movement.x < 0)
+        {
+            Flip(true);
+        }
+        else if (movement.x > 0)
+        {
+            Flip(false);
+        }
+    }
+
+
+    private void Flip(bool value)
+    {
+        if (value)
+        {
+            transform.localScale = new Vector3(-originalScale.x, originalScale.y, originalScale.z);
+        }
+        else
+        {
+            transform.localScale = originalScale;
+        }
+    }
+
+    public void ToggleMovement(bool value)
+    {
+        canMove = value;
+    }
+}
